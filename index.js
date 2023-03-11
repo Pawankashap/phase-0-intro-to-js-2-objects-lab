fef29f4ac65ff7a406b2ce5abb48b1ee325a3980
Fnu Pawan Kumar <pawan.3940@gmail.com>
2023-03-11 02:33:26 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee={name:"Pawan Kumar",streetAddress:"Union City, California, USA"}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = { ...obj };
+    newObj[key] = value; //Why are we using bracket notation here?
+    return newObj;
+  }
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj,key,value) {
+    obj[key] = value; //Why are we using bracket notation here?
+    return obj;
+}
+ 
+function deleteFromEmployeeByKey (obj,key,value) {
+    const newObj= { ...obj}
+    delete newObj[key]
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj,key) {
+    delete obj[key]
+    return obj;
+}
